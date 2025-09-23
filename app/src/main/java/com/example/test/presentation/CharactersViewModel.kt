364fb1fd18d93b3719a1faf24993711f8ce68159
adelina.aidarbekova <adelina.aidarbekova@optimabank.kg>
2025-09-23 15:23:26 +0600

    Add context for coroutine in viewmodel; Fix loading on fetching error

--- app/src/main/java/com/example/test/presentation/CharactersViewModel.kt
+++ app/src/main/java/com/example/test/presentation/CharactersViewModel.kt
@@ -3,6 +3,7 @@ package com.example.test.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test.domain.usecase.GetCharactersUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -21,14 +22,14 @@ class CharactersViewModel(
 	}
 	
 	private fun loadCharacters() {
-		viewModelScope.launch {
+		viewModelScope.launch(Dispatchers.IO) {
 			_state.update { it.copy(isLoading = true, error = null) }
 			getCharactersUseCase.invoke()
 				.onSuccess { result ->
 					_state.update { it.copy(isLoading = false, charactersList = result) }
 				}
 				.onFailure { error ->
-					_state.update { it.copy(error = error.message) }
+					_state.update { it.copy(isLoading = false, error = error.message) }
 				}
 		}
 	}
